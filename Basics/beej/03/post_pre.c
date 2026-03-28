2282d4415dc316a875ee71d40d6106b46207cc5e
rudv-ar <rudv-ar.base@gmail.com>
2026-03-29 00:08:09 +0530

    add: [pre, postfix operators]

---
+++ Basics/beej/03/post_pre.c
@@ -0,0 +1,5 @@
+// the Post and Pre increment or decrements...
+// it is just self instructing : ++i means first increment i, then return i 
+// i++ means : first return i, then increment i 
+// --i : first decrement, then return i 
+// i-- : first return, then decrement i
