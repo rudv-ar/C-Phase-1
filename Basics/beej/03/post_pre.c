207cd26dfa55bbbf3f5edfecf7452510109008b7
rudv-ar <rudv-ar.base@gmail.com>
2026-03-29 11:05:33 +0530

    add: [pre, postfix operators, init]

--- Basics/beej/03/post_pre.c
+++ Basics/beej/03/post_pre.c
@@ -3,3 +3,19 @@
 // i++ means : first return i, then increment i 
 // --i : first decrement, then return i 
 // i-- : first return, then decrement i
+#include <stdio.h>
+
+int main(void) {
+    int x = 99;
+    printf("Before prefix increment x : %d\n", x);
+    int y = ++x; 
+    // increment x, x = 100, return x, y = 100.
+    printf("After prefix increment x : %d\n", x);
+    printf("After prefix increment of x ; value of y = ++x : %d\n", y);
+    int z = 100 + ++x;
+    // increment x again : 101 now, return x, 100 + 101 = 201; this is value of z.
+    printf("After the second prefix increment of x , value of z : %d\n", z);
+    z = ++x - ++y;
+    // increment x again, 102 now, return x, increment y, now y is 101. subtract them both.
+    printf("The value of z = x - y = %d - %d = %d\n", x, y, z);
+}
